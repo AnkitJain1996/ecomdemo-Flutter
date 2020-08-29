fef66035dfc6c9fb82f69819396757a4902f8477
Ankit Jain <ankitjain.aj1996@google.com>
2020-08-29 21:18:30 +0530

    aaa

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Ecom demo Application',
+      title: 'Pushed to Github',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
